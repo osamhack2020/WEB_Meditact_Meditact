c52f98a97a1ad21c0e3ccb11dba13d3000493289
찬호 김 <hpyho33@naver.com>
2020-10-23 14:44:30 +0000

    로그인 상태의 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { Route, Switch,  } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+
 import 
 { 
   MainPage,
@@ -15,18 +18,28 @@ import
 
 
 
-const App = ()=>(
-  <div>
-    <Route exact path="/" component={MainPage}/>
-    <Route exact path="/login" component={LoginPage}/>
-    <Route exact path="/reservation" component={ReservationPage}/>
-    <Route exact path="/chatbot" component={ChatBotPage}/>
-    <Route exact path="/mypage" component={MypagePage}/>
-    <Route exact path="/signup" component={SignupPage}/>
-    <Route exact path="/information" component={InformationPage}/>
-
-  </div>
-)
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/reservation:id" component={ReservationPage} />
+        <Route exact path="/chatbot" component={ChatBotPage} />
+        <Route exact path="/mypage:id" component={MypagePage} />
+        <Route exact path="/signup" component={SignupPage} />
+        <Route exact path="/information" component={InformationPage} />
+      </div>
+    )
+  }
+}
+// user객체 가져와서 id로 url 구분하기
+let mapStateToProps = (state) => {
+  return {
+      isLogin: state.user.isLogin
+  };
+}
+App = connect(mapStateToProps)(App);
 
 
 export default App;
